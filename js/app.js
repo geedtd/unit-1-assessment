7dfb2df9dc40c00c416e7bb07cb54517a41528ca
geedtd <grcazares@gmail.com>
2021-06-30 07:17:21 -1000

    Reached MVP and did 2 easy bonuses

--- js/app.js
+++ js/app.js
@@ -2,14 +2,29 @@ const result = document.getElementById('result')
 const minus = document.getElementById('minus')
 const plus = document.getElementById('plus')
 const input = document.getElementById('num-input')
+const reset = document.getElementById('reset')
 
 plus.addEventListener('click', handlePlus)
 minus.addEventListener('click', handleMinus)
+reset.addEventListener('click', handleReset)
 
 function handleMinus()  {
     result.innerText -= parseInt(input.value)
+    if (parseInt(result.innerText) < 0) {
+        result.style.color = 'red'
+    }
 
 }
 function handlePlus()   {
     result.innerText = parseInt(input.value) + parseInt(result.innerText)
+    if (parseInt(result.innerText) >= 0) {
+        result.style.color = 'black'
+    }
+}
+
+function handleReset() {
+    console.log('party')
+    result.style.color = 'black'
+    result.innerText = 0
+
 }
