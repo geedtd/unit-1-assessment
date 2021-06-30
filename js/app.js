bc3fd9a22040010991e8b0309e089f9f85947b88
geedtd <grcazares@gmail.com>
2021-06-30 06:42:47 -1000

    added eventListeners, started on function for addition and substraction

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const result = document.getElementById('result')
+const minus = document.getElementById('minus')
+const plus = document.getElementById('plus')
+const input = document.getElementById('num-input')
+
+plus.addEventListener('click', handlePlus)
+minus.addEventListener('click', handleMinus)
+
+function handleMinus()  {
+    console.log('min')
+
+}
+function handlePlus()   {
+    console.log('plus')
+}
